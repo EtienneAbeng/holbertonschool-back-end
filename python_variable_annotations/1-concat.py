60e4234beac239339b0e4345cc2b118120793355
EtienneAbeng <abengetienne@gmail.com>
2024-04-24 23:19:59 +0200

    Deplace to holbertonschool_web_back_end

--- python_variable_annotations/1-concat.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-"""A type-annotated function concat that takes string str1 and string str2"""
-
-
-def concat(str1: str, str2: str) -> str:  # type hinting
-    """ Concatenate two strings and return the result """
-    return str1 + str2
